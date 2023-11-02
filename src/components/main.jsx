530c3c8e6959fb2e623f4a024acc3c915406c984
HiteshDatt <hdk.hitesh26@gmail.com>
2023-11-02 14:45:44 +0530

    app setup completed

---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+export function Main(props) {
+  return (
+    <>
+      <div>Final</div>
+    </>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,36 @@
-import { render } from 'preact';
-import style from './shadow-style.css?inline';
+import { render } from "preact";
+import style from "./shadow-style.css?inline";
+import { Main } from "./components/main";
 
 export function App() {
-	return (
-		<>
-		<div class="widget-container">hiiiiiiiiiiii actual app</div>
-		<div class="widget-styles"></div>
-		</>
-	);
+  return (
+    <>
+      <div class="widget-container">
+        <Main />
+      </div>
+      <div class="widget-styles"></div>
+    </>
+  );
 }
 
 export function AppCSS() {
-	return (
-		<style>
-			{style}
-		</style>
-	);
+  return <style>{style}</style>;
 }
-
 
 // Load the App content into the shadow DOM
 let shadowTarget = document.createElement("div");
 shadowTarget.className = "fc-wallet-cart-widget-19212-target";
-shadowTarget.style.display='block';
-document.getElementById('fc-wallet-cart-widget-19212').appendChild(shadowTarget);
+shadowTarget.style.display = "block";
+document
+  .getElementById("fc-wallet-cart-widget-19212")
+  .appendChild(shadowTarget);
 let shadow = shadowTarget.attachShadow({ mode: "open" });
 let shadowRoot = document.createElement("div");
 shadowRoot.className = "fc-wallet-cart-widget-19212-root";
 shadow.appendChild(shadowRoot);
 
-
 render(<App />, shadowRoot);
 render(<AppCSS />, shadowRoot?.querySelector(".widget-styles"));
 
-
 //render(<App />, document.getElementById('fc-wallet-cart-widget-19212'));
 //render(<AppCSS />, document.getElementById('fc-wallet-cart-widget-19212')?.querySelector(".widget-styles"));
-
-
-
