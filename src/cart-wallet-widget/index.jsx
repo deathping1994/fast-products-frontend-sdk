a84baab1f291e0fa2ea603dba011837e66f92ca8
HiteshDatt <hdk.hitesh26@gmail.com>
2023-11-06 17:10:00 +0530

    init function recalled multiple times

--- src/cart-wallet-widget/index.jsx
+++ src/cart-wallet-widget/index.jsx
@@ -19,21 +19,21 @@ export function AppCSS() {
 
 export const WALLET_API_URI = import.meta.env.VITE_APP_BASE_URL;
 
-function renderWalletBox(){
-// Load the App content into the shadow DOM
-let shadowTarget = document.createElement("div");
-shadowTarget.className = "fc-wallet-cart-widget-19212-target";
-shadowTarget.style.display = "block";
-document
-  .getElementById("fc-wallet-cart-widget-19212")
-  .appendChild(shadowTarget);
-let shadow = shadowTarget.attachShadow({ mode: "open" });
-let shadowRoot = document.createElement("div");
-shadowRoot.className = "fc-wallet-cart-widget-19212-root";
-shadow.appendChild(shadowRoot);
+function renderWalletBox() {
+  const targetDiv = document.getElementById("fc-wallet-cart-widget-19212");
+  targetDiv.innerHTML = "";
+  // Load the App content into the shadow DOM
+  let shadowTarget = document.createElement("div");
+  shadowTarget.className = "fc-wallet-cart-widget-19212-target";
+  shadowTarget.style.display = "block";
+  targetDiv.appendChild(shadowTarget);
+  let shadow = shadowTarget.attachShadow({ mode: "open" });
+  let shadowRoot = document.createElement("div");
+  shadowRoot.className = "fc-wallet-cart-widget-19212-root";
+  shadow.appendChild(shadowRoot);
 
-render(<App />, shadowRoot);
-render(<AppCSS />, shadowRoot?.querySelector(".widget-styles"));
+  render(<App />, shadowRoot);
+  render(<AppCSS />, shadowRoot?.querySelector(".widget-styles"));
 }
 
 // @ts-ignore
