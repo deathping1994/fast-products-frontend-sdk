d10b9749ddd03f06e30f076c1bd62e3e4da5bd62
kaseraSwastik <swastik.k@brimo.in>
2024-01-17 14:37:09 +0530

    Popup Added

--- src/loyalty-popup/components/main.jsx
+++ src/loyalty-popup/components/main.jsx
@@ -2,10 +2,28 @@ import { useEffect, useState } from "preact/hooks";
 import { WALLET_API_URI } from "..";
 
 export function Main({ themeDetailsData }) {
-
+  const [visibilty, setVisibility] = useState(false)
+  const handleViewPopup = ()=>{
+      setVisibility(!visibilty)
+  }
   return (
     <>
-      hello
+      <img onClick={handleViewPopup} class="floatingPopup" src="https://media.farziengineer.co/farziwallet/gift-icon.png" width={34} height={34} alt="gift icon" />
+      {visibilty && (
+        <>
+          <div class="mainPopup">
+            <div class="header">
+                <div>
+                    <p>Welcome to</p>
+                    <h6>Loyality</h6>
+                </div>
+                <div>
+                    <img class="closePopup" onClick={handleViewPopup} src="https://media.farziengineer.co/farziwallet/cross.png" alt="" />
+                </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
