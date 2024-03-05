722ec759321e507c7ae832f9474ad3863a4a38f0
kaseraSwastik <swastik.k@brimo.in>
2024-03-05 19:11:44 +0530

    Referral popup fix

--- src/loyalty-popup/components/main.jsx
+++ src/loyalty-popup/components/main.jsx
@@ -124,7 +124,7 @@ export function Main({ themeDetailsData, shadowRoot }) {
         } catch (err) {
           console.log("error in redeemReferHash", err);
         }
-      }, 3000);
+      }, 2000);
     }
   }  
 
