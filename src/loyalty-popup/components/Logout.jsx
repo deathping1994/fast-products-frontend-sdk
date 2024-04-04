984c6e35b276bc715e29958da53dc4b9cef3a45f
kaseraSwastik <swastik.k@brimo.in>
2024-04-04 11:41:48 +0530

    comment out logs

--- src/loyalty-popup/components/Logout.jsx
+++ src/loyalty-popup/components/Logout.jsx
@@ -4,7 +4,9 @@ const Logout = ({handleLogin}) => {
     <>
         <div class="logoutwalletCard">
             <div>
-                <p class="walletCardText">My {window.fc_loyalty_vars.coin_name} Coins</p>
+                <p class="walletCardText">My {window.
+// @ts-ignore
+                fc_loyalty_vars.coin_name} Coins</p>
                 <div class="badgeCard">
                     <img src="https://media.farziengineer.co/farziwallet/silver-tier-icon.png" alt="" />
                     <p>Silver</p>
