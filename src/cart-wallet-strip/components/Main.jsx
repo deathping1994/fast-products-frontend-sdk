c3211b3e4a125606363ec4ed9dd36205d60f3349
kaseraSwastik <swastik.k@brimo.in>
2024-05-13 15:43:06 +0530

    cart-wallet-strip completed

---
+++ src/cart-wallet-strip/components/Main.jsx
@@ -0,0 +1,173 @@
+import { useEffect, useState } from "preact/hooks";
+import { WALLET_API_URI } from "..";
+
+const Main = ({themeDetailsData, shadowRoot}) => {
+    const [cashbackDetails, setCashbackDetails] = useState({
+        amount: 0,
+        type: null,
+    });
+    const [walletAppliedDetails, setWalletAppliedDetails] = useState({
+        currency: null,
+        totalPayablePrice: 0,
+        totalDiscount: 0,
+    });
+    const [customerDetails, setCustomerDetails] = useState({
+        customerID: "",
+        customerTags: "",
+        clientID: "",
+    });
+    const [checkoutTarget, setCheckoutTarget] = useState({
+        enable: false,
+        isSet: false,
+    });
+    const [refetchCartSummary, setRefetchSummary] = useState(false);
+    const [cashbackAmount, setCashbackAmount] = useState(0);
+    const [loadingCashbackDetails, setLoadingCashbackDetails] = useState(true);
+    const [appliedDiscountsList, setAppliedDiscountsList] = useState(null);
+    const [appliedDiscountCode, setAppliedDiscountCode] = useState("");
+    const calculateCashback = ({ totalPrice }) => {
+        if (cashbackDetails?.type === "percent") {
+            // @ts-ignore
+            const cartAmount = window.fc_cart_details?.totalPayablePrice;
+            const finalAmount =
+            (cashbackDetails?.amount / 100) * (totalPrice || cartAmount);
+            setCashbackAmount(finalAmount);
+            return finalAmount;
+        } else {
+            setCashbackAmount(cashbackDetails?.amount);
+            return cashbackDetails?.amount;
+        }
+    };
+    const loadCartSummary = async () => {
+        const cartRes = await fetch(`/cart.json?v=${Date.now()}`);
+        const cartDetails = await cartRes.json();
+        const totalPrice = cartDetails?.total_price / 100;
+        const totalDiscount = cartDetails?.total_discount / 100;
+        const appliedDiscountCode =
+          cartDetails?.cart_level_discount_applications?.find((item) => {
+            return (
+              item.type === "discount_code" && !item.title.includes("WALLETAPPLIED")
+            );
+          })?.title || "";
+    
+        setAppliedDiscountsList(cartDetails?.cart_level_discount_applications);
+        setWalletAppliedDetails({
+          currency: cartDetails?.currency,
+          totalPayablePrice: Number(totalPrice),
+          totalDiscount: Number(totalDiscount),
+        });
+        // @ts-ignore
+        window.fc_cart_details = {
+          totalPayablePrice: Number(totalPrice),
+          totalDiscount: Number(totalDiscount),
+        };
+        setAppliedDiscountCode(appliedDiscountCode);
+        calculateCashback({
+          totalPrice: Number(totalPrice),
+        });
+      };
+      const syncCartSummary = async (walletAppliedDetails) => {
+        const cartRes = await fetch(`/cart.json?v=${Date.now()}`);
+        const cartDetails = await cartRes.json();
+        const totalPrice = cartDetails?.total_price / 100;
+        const totalDiscount = cartDetails?.total_discount / 100;
+        window.fc_cart_details = {
+          totalPayablePrice: Number(totalPrice),
+          totalDiscount: Number(totalDiscount),
+        };
+        if (
+          totalPrice === window.fc_cart_details?.totalPayablePrice &&
+          totalDiscount === window.fc_cart_details?.totalDiscount
+        ) {
+          //already updated
+        } else {
+          setRefetchSummary((prev) => !prev);
+          console.log("ELSE synccart");
+        }
+      };
+      const getCashbackDetails = async ({ customerID, customerTags, clientID }) => {
+        setLoadingCashbackDetails(true);
+        const cashbackRes = await fetch(
+          `${WALLET_API_URI}/loyalty/cashback-detail`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              customer_id: customerID,
+              user_hash: customerTags,
+              client_id: clientID,
+            }),
+          }
+        );
+        const cashbackResDetails = await cashbackRes.json();
+        setCashbackDetails({
+          type: cashbackResDetails?.data?.cashback_data?.type,
+          amount: Number(cashbackResDetails?.data?.cashback_data?.amount),
+        });
+    
+        setLoadingCashbackDetails(false);
+      };
+      useEffect(() => {
+        const mainScript = document.querySelector(
+          "#fc-wallet-cart-strip-script-19212"
+        );
+        const customer_id = mainScript.getAttribute("data-customer-id");
+        const customer_tags = mainScript.getAttribute("data-customer-tag")?.trim() ;
+        const client_id = mainScript.getAttribute("data-client-id");
+    
+        const checkout_target = mainScript.getAttribute("data-checkout-target");
+        const coupon_code_box = mainScript.getAttribute("data-coupon-code-box");
+        const cashback_strip = mainScript.getAttribute("data-cashback-strip");
+        const wallet_credit = mainScript.getAttribute("data-wallet-credit-box");
+        const walletUiTheme = mainScript.getAttribute('wallet-theme')
+    
+        if (checkout_target) {
+          setCheckoutTarget({
+            enable: true,
+            isSet: true,
+          });
+        } else {
+          setInterval(() => {
+            // @ts-ignore
+            syncCartSummary(window.fc_cart_details || walletAppliedDetails);
+          }, 10000);
+          setCheckoutTarget({
+            enable: false,
+            isSet: true,
+          });
+        }
+    
+        setCustomerDetails({
+          customerID: customer_id,
+          customerTags: customer_tags || sessionStorage.getItem("fc_wallet_user_hash") || '',
+          clientID: client_id,
+        });
+    
+        getCashbackDetails({
+          customerID: customer_id,
+          customerTags: customer_tags,
+          clientID: client_id,
+        });
+      }, []);
+      useEffect(() => {
+        loadCartSummary();
+      }, [refetchCartSummary, cashbackDetails?.type]);
+  return (
+    <>
+        {!loadingCashbackDetails && cashbackAmount !== 0 && (
+        <>
+        <div class="cashback-strip-container">
+          <p>
+            You'll get&nbsp;<span>Rs. {parseFloat(`${cashbackAmount}`).toFixed(2)} cashback</span>
+            &nbsp;with this order
+          </p>
+        </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default Main
---
+++ src/cart-wallet-strip/index.jsx
@@ -0,0 +1,100 @@
+import { render } from "preact";
+import style from "./shadow-style.css?inline";
+import Main from "./components/Main";
+
+export function App({ themeDetailsData, shadowRoot }) {
+  return (
+    <>
+      <div class="widget-container">
+        <Main themeDetailsData={themeDetailsData} shadowRoot={shadowRoot} />
+      </div>
+      <div class="widget-styles"></div>
+      <div class="widget-custom-styles"></div>
+    </>
+  );
+}
+
+export function AppCSS() {
+  return <style>{style}</style>;
+}
+
+export function AppCustomCSS({ customStyles }) {
+  return <style>{customStyles}</style>;
+}
+
+export const WALLET_API_URI = import.meta.env.VITE_APP_BASE_URL;
+
+async function renderWalletStrip() {
+  try {
+    const targetDiv = document.getElementById("fc-wallet-cart-strip-19212");
+    targetDiv.innerHTML = "";
+    // Load the App content into the shadow DOM
+    let shadowTarget = document.createElement("div");
+    shadowTarget.className = "fc-wallet-cart-strip-19212-target";
+    shadowTarget.style.display = "block";
+    targetDiv.appendChild(shadowTarget);
+    let shadow = shadowTarget.attachShadow({ mode: "open" });
+    let shadowRoot = document.createElement("div");
+    shadowRoot.className = "fc-wallet-cart-strip-19212-root";
+    shadow.appendChild(shadowRoot);
+
+    let themeDetailsData;
+    // @ts-ignore
+    if (window?.fc_loyalty_vars?.theme_details) {
+      // @ts-ignore
+      themeDetailsData = window.fc_loyalty_vars.theme_details;
+    } else {
+      const mainScript = document.querySelector(
+        "#fc-wallet-cart-strip-script-19212"
+      );
+      const client_id = mainScript.getAttribute("data-client-id");
+      const themeDetailsRes = await fetch(
+        `${WALLET_API_URI}/get-theme-details`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            client_id: client_id,
+          }),
+        }
+      );
+      themeDetailsData = await themeDetailsRes.json();
+      // @ts-ignore
+      window.fc_loyalty_vars = { theme_details: themeDetailsData };
+    }
+    const clientCustomStyleData =
+      themeDetailsData?.data?.apply_wallet_snippet_css || "";
+
+    render(<App themeDetailsData={themeDetailsData} shadowRoot={shadowRoot} />, shadowRoot);
+    render(<AppCSS />, shadowRoot?.querySelector(".widget-styles"));
+    render(
+      <AppCustomCSS customStyles={clientCustomStyleData} />,
+      shadowRoot?.querySelector(".widget-custom-styles")
+    );
+  } catch (err) {
+    console.log("error", err);
+  }
+}
+
+function checkWalletStrip() {
+    setInterval(() => {
+      if (
+        document.querySelector(
+          "#fc-wallet-cart-strip-19212 .fc-wallet-cart-strip-19212-target"
+        )
+      ) {
+        //already rendered
+      } else {
+        //trigger render
+        renderWalletStrip();
+      }
+    }, 300);
+  }
+  // @ts-ignore
+  window.fc_loyalty_check_wallet_box = checkWalletStrip;
+  checkWalletStrip();
+// @ts-ignore
+window.fc_loyalty_render_wallet_box = renderWalletStrip; //Exposing for external use
+renderWalletStrip(); // Calling on first load
