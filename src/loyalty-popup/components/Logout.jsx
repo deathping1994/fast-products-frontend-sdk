597f8fe11f9c6c5e19c178abf02b0ae109bd4df7
kaseraSwastik <swastik.k@brimo.in>
2024-05-16 17:49:44 +0530

    coins text removed

--- src/loyalty-popup/components/Logout.jsx
+++ src/loyalty-popup/components/Logout.jsx
@@ -6,7 +6,7 @@ const Logout = ({handleLogin}) => {
             <div>
                 <p class="walletCardText">My {window.
 // @ts-ignore
-                fc_loyalty_vars.coin_name} Coins</p>
+                fc_loyalty_vars.coin_name}</p>
                 <div class="badgeCard">
                     <img src="https://media.farziengineer.co/farziwallet/silver-tier-icon.png" alt="" />
                     <p>Silver</p>
