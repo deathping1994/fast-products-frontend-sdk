3a3777dd2585d78caea5151d9f410b01e6ebbff8
avt-brimo <amit.t@brimo.in>
2024-07-01 13:16:42 +0530

    created a global function that will take a function and will call it whenever cart is refreshed

--- src/cart-wallet-widget/components/ModernUI/ModernMain.jsx
+++ src/cart-wallet-widget/components/ModernUI/ModernMain.jsx
@@ -1,6 +1,20 @@
 import {useState} from "preact/hooks";
 import ModernLogin from "./ModernLogin";
 
+// this function will take a function and will debounce call the function with delay provided.
+function debounceCallTheFunction(func, delay) {
+  let timerId;
+  return function(...args) {
+      const context = this;
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+          func.apply(context, args);
+      }, delay);
+  };
+}
+// we are creating a global function that can be called from anywhere and it will take a function and will call it every time the cart is refreshed. We are debouncing that function as there are too many rerenders so for performance prospective.
+const debouncedcall = debounceCallTheFunction(()=>{window.fc_callFuncOnRefresh()}, 1000);
+
 const ModernMain = ({
   themeDetailsData,
   walletAppliedDetails,
@@ -26,12 +40,15 @@ const ModernMain = ({
 // When a coupon is already applied then our coupon don't gets applied with other coupon so in that case the below function will uncheck the checkbox
  function checkForCheckbox(){
     if( walletAppliedDetails.walletDiscountApplied==0){
+      debouncedcall()
       setIsWalletApplied(false)
     }else{
+      debouncedcall()
       setIsWalletApplied(true)
     }
   }  
   checkForCheckbox()
+  console.log("bebo amin");
   return (
     <>
       {
